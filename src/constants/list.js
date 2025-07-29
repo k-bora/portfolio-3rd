c2de8ea89f78573ab2eaa7a0ad40ee9e354633ef
kimbora <k.bora9322@gmail.com>
2025-07-29 18:03:58 +0900

    피그마 추가

--- src/constants/list.js
+++ src/constants/list.js
@@ -1,81 +1,82 @@
 export const headerNav = [
-    {
-        title: "HOME",
-        url: "#home",
-    },
-    {
-        title: "ABOUT",
-        url: "#about",
-    },
-    {
-        title: "PORTFOLIO",
-        url: "#port",
-    },
-    {
-        title: "CONTACT",
-        url: "#contact",
-    },
+  {
+    title: "HOME",
+    url: "#home",
+  },
+  {
+    title: "ABOUT",
+    url: "#about",
+  },
+  {
+    title: "PORTFOLIO",
+    url: "#port",
+  },
+  {
+    title: "CONTACT",
+    url: "#contact",
+  },
 ];
 
 export const workList = [
-    {
-        startDate: "2021.07",
-        endDate: "2024.10",
-        title: "emgram",
-        text: "응용 소프트웨어 개발 및 공급업",
-    },
-    {
-        startDate: "2020.12",
-        endDate: "2021.06",
-        title: "49week",
-        text: "웹에이전시",
-    }
+  {
+    startDate: "2021.07",
+    endDate: "2024.10",
+    title: "emgram",
+    text: "응용 소프트웨어 개발 및 공급업",
+  },
+  {
+    startDate: "2020.12",
+    endDate: "2021.06",
+    title: "49week",
+    text: "웹에이전시",
+  },
 ];
 
 export const eduList = [
-    {
-        startDate: "2021.09",
-        endDate: "2024.09",
-        title: "한국방송통신대학교",
-        text: "컴퓨터과학과",
-    },
-    {
-        startDate: "2020.03",
-        endDate: "2020.08",
-        title: "경기산업기술교육센터",
-        text: "웹퍼블리셔과정",
-    },
-    {
-        startDate: "2012.02",
-        endDate: "2015.02",
-        title: "경복대학교",
-        text: "치위생과",
-    },
+  {
+    startDate: "2021.09",
+    endDate: "2024.09",
+    title: "한국방송통신대학교",
+    text: "컴퓨터과학과",
+  },
+  {
+    startDate: "2020.03",
+    endDate: "2020.08",
+    title: "경기산업기술교육센터",
+    text: "웹퍼블리셔과정",
+  },
+  {
+    startDate: "2012.02",
+    endDate: "2015.02",
+    title: "경복대학교",
+    text: "치위생과",
+  },
 ];
 
 export const licenseList = [
-    {
-        date: "2021.09",
-        name: "웹디자인기능사",
-        institution: "한국산업인력공단",
-    },
-    {
-        date: "2024.00",
-        name: "정보처리기사",
-        institution: "한국산업인력공단",
-    },
+  {
+    date: "2021.09",
+    name: "웹디자인기능사",
+    institution: "한국산업인력공단",
+  },
+  {
+    date: "2024.00",
+    name: "정보처리기사",
+    institution: "한국산업인력공단",
+  },
 ];
 
 export const skillList = [
-    {name: "Html",},
-    {name: "Css",},
-    {name: "Scss",},
-    {name: "Javascript",},
-    {name: "JQuery",},
-    {name: "Git",},
-    {name: "Sourcetree",},
-    {name: "Photoshop",},
-    {name: "Illustrator",},
-    {name: "Xd",},
-    {name: "Zeplin",},
+  { name: "Html" },
+  { name: "Css" },
+  { name: "Scss" },
+  { name: "Javascript" },
+  { name: "JQuery" },
+  { name: "Git" },
+  { name: "Sourcetree" },
+  { name: "Photoshop" },
+  { name: "Illustrator" },
+  { name: "Xd" },
+  { name: "Zeplin" },
+  { name: "Figma" },
 ];
